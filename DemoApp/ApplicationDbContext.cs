1fae5ad3a8aee250255ad085187da1473fbf4e0f
shanmunir <shaanuet@gmail.com>
2025-07-22 16:00:46 +0500

    Initial Azure migration changes

---
+++ DemoApp/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Example entity sets:
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    // Add your other DbSets here
+
+    // Optional: configure model
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Example configuration:
+        modelBuilder.Entity<WeatherForecast>()
+            .HasKey(w => w.Id);
+    }
+}
+
+// Sample entity
+public class WeatherForecast
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string Summary { get; set; }
+}
--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,10 +1,14 @@
 using DemoApp.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -15,7 +19,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
